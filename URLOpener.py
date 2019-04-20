b1d9b1bd9003c6327575146a09bef9c798fa7580
onlinePB <47390291+onlinePB@users.noreply.github.com>
2019-04-20 11:00:09 +0100

    Added more print statements Added more print statements, to give the user the url for the issues page.

--- URLOpener.py
+++ URLOpener.py
@@ -5,11 +5,13 @@
 import webbrowser
 import sys
 
+print("URLOpener version 1.0.0 (https://github.com/onlinePB/URL_Opener)\nBy Ryan W (https://github.com/onlinePB)")
+
 # Try to open the file
 try:
     urls = open("urls.txt", "r")
 except FileNotFoundError:
-    print("ERROR: urls.txt not found.")
+    print("\nERROR: urls.txt not found.\nIs this a bug? Report it here: https://github.com/onlinePB/URL_Opener/issues")
     input("Press enter to continue...")
     sys.exit()
 
