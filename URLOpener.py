899efa69eaf9db6c7c628bb56f06cda78a35ed42
onlinePB <47390291+onlinePB@users.noreply.github.com>
2019-04-20 10:37:51 +0100

    Implemented code Created the code to open the file, read the file and open every URL in the file.

--- URLOpener.py
+++ URLOpener.py
@@ -1,2 +1,21 @@
 # Ryan W (https://github.com/onlinePB)
 # URL Opener (https://github.com/onlinePB/URL_Opener)
+# Version 1.0.0
+
+import webbrowser
+import sys
+
+# Try to open the file
+try:
+    urls = open("urls.txt", "r")
+except FileNotFoundError:
+    print("ERROR: urls.txt not found.")
+    input("Press enter to continue...")
+    sys.exit()
+
+# Open URLs
+for line in urls:
+    webbrowser.open(line, new=0, autoraise=True)
+
+# Close the file
+urls.close()
